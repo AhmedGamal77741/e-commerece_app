f997dbfed89572501735a31376852335b5bc0f7e
MohamedAliBadawy <medooali6262@gmail.com>
2025-05-14 16:50:31 +0300

    add loading dialog to add_post screen

--- lib/features/home/add_post.dart
+++ lib/features/home/add_post.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerece_app/core/helpers/loading_dialog.dart';
 import 'package:ecommerece_app/core/helpers/spacing.dart';
 import 'package:ecommerece_app/core/theming/colors.dart';
 import 'package:ecommerece_app/core/theming/styles.dart';
@@ -20,7 +21,6 @@ class _AddPostState extends State<AddPost> {
   String imgUrl = "";
   final currentUser = FirebaseAuth.instance.currentUser;
   TextEditingController _textController = TextEditingController();
-  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -30,15 +30,18 @@ class _AddPostState extends State<AddPost> {
           onPressed: () async {
             if (_textController.text.isEmpty && imgUrl.isEmpty) return;
 
+            showLoadingDialog(context);
             try {
               await uploadPost(text: _textController.text, imgUrl: imgUrl);
+              Navigator.pop(context);
               ScaffoldMessenger.of(
                 context,
               ).showSnackBar(SnackBar(content: Text('성공')));
+
               Navigator.pop(context);
             } catch (e) {
               print(e.toString());
-
+              Navigator.pop(context);
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('게시물 게시에 실패했습니다: ${e.toString()}')),
               );
@@ -90,94 +93,92 @@ class _AddPostState extends State<AddPost> {
             child: Container(color: ColorsManager.primary100, height: 1.0.h),
           ),
         ),
-        body:
-            _isLoading
-                ? Center(child: CircularProgressIndicator(color: Colors.black))
-                : Column(
-                  children: [
-                    verticalSpace(20),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Flexible(
-                          child: Container(
-                            width: 56.w,
-                            height: 55.h,
-                            decoration: ShapeDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(currentUser!.photoURL!),
-                                fit: BoxFit.cover,
-                              ),
-                              shape: OvalBorder(),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 4,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            spacing: 15.h,
-                            children: [
-                              Text(
-                                currentUser!.displayName!,
-                                style: TextStyles.abeezee16px400wPblack,
-                              ),
-                              TextField(
-                                controller: _textController,
-                                onChanged: (value) {
-                                  setState(() {}); // Handle text changes
-                                },
-                                decoration: InputDecoration(
-                                  hintText: '새로운 소식이 있나요?', // Placeholder text
-                                  border:
-                                      InputBorder.none, // Removes all borders
-                                  contentPadding:
-                                      EdgeInsets
-                                          .zero, // Removes default padding
-                                  isDense: true, // Reduces vertical padding
-                                  hintStyle: TextStyle(
-                                    // Matches your text style
-                                    color: const Color(0xFF5F5F5F),
-                                    fontSize: 13.sp,
-                                    fontFamily: 'NotoSans',
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                                style: TextStyle(
-                                  color: const Color(0xFF5F5F5F),
-                                  fontSize: 13.sp,
-                                  fontFamily: 'NotoSans',
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () async {
-                                  imgUrl = await uploadImageToImgBB();
-                                  setState(() {});
-                                },
-                                child:
-                                    imgUrl.isEmpty
-                                        ? ImageIcon(
-                                          AssetImage('assets/image_icon.png'),
-                                          size: 17.sp,
-                                        )
-                                        : Image.network(
-                                          imgUrl,
-                                          height: 272.h,
-                                          width: 200.w,
-                                          fit: BoxFit.cover,
-                                        ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+        body: Column(
+          children: [
+            verticalSpace(20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Flexible(
+                  child: Container(
+                    width: 56.w,
+                    height: 55.h,
+                    decoration: ShapeDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(currentUser!.photoURL!),
+                        fit: BoxFit.cover,
+                      ),
+                      shape: OvalBorder(),
                     ),
-                  ],
+                  ),
                 ),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 15.h,
+                    children: [
+                      Text(
+                        currentUser!.displayName!,
+                        style: TextStyles.abeezee16px400wPblack,
+                      ),
+                      TextField(
+                        controller: _textController,
+                        onChanged: (value) {
+                          setState(() {}); // Handle text changes
+                        },
+                        decoration: InputDecoration(
+                          hintText: '새로운 소식이 있나요?', // Placeholder text
+                          border: InputBorder.none, // Removes all borders
+                          contentPadding:
+                              EdgeInsets.zero, // Removes default padding
+                          isDense: true, // Reduces vertical padding
+                          hintStyle: TextStyle(
+                            // Matches your text style
+                            color: const Color(0xFF5F5F5F),
+                            fontSize: 13.sp,
+                            fontFamily: 'NotoSans',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        style: TextStyle(
+                          color: const Color(0xFF5F5F5F),
+                          fontSize: 13.sp,
+                          fontFamily: 'NotoSans',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          showLoadingDialog(context);
+
+                          imgUrl = await uploadImageToImgBB();
+                          Navigator.pop(context);
+                          setState(() {});
+                        },
+                        child:
+                            imgUrl.isEmpty
+                                ? ImageIcon(
+                                  AssetImage('assets/image_icon.png'),
+                                  size: 17.sp,
+                                )
+                                : Image.network(
+                                  imgUrl,
+                                  height: 272.h,
+                                  width: 200.w,
+                                  fit: BoxFit.cover,
+                                ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
