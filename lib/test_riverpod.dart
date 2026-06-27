d95d79f0f61e9a932cde72cacaa14a6e4566df61
AhmedGamal77741 <62484621+AhmedGamal77741@users.noreply.github.com>
2026-06-27 16:14:05 +0300

    Remove unused test_riverpod.dart

--- lib/test_riverpod.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final testProvider = AsyncNotifierProviderFamily<TestNotifier, int, String>(TestNotifier.new);
-
-class TestNotifier extends FamilyAsyncNotifier<int, String> {
-  @override
-  FutureOr<int> build(String arg) => 0;
-}
